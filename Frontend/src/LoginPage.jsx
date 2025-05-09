26fb7e7d43dbd6fa47f1359e5d3be7f969efc27f
Leonardo Tagliati <leotagliati56@gmail.com>
2025-05-09 19:08:50 -0300

    feat: esqueleto inicial

--- Frontend/src/LoginPage.jsx
+++ Frontend/src/LoginPage.jsx
@@ -1,4 +1,8 @@
 
+import { Button } from 'primereact/button'
+import { FloatLabel } from 'primereact/floatlabel';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
 import React from 'react'
 
 export const LoginPage = () => {
@@ -12,10 +16,20 @@ export const LoginPage = () => {
                         className="img-fluid h-100 w-100 object-fit-cover"
                     />
                 </div>
-
                 <div className="col-md-7 d-flex flex-column justify-content-center align-items-center bg-light">
                     <h2 className="mb-4">Bem-vindo de volta</h2>
                     <p>Por favor, entre com suas credenciais para continuar.</p>
+                    <div className='d-flex flex-column justify-content-center gap-3 pb-4'>
+                        <InputText placeholder='Username' />
+                        <Password placeholder='Password' />
+
+                    </div>
+                    <div className='d-flex flex-column-2 gap-2 mb-4'>
+                        <Button label='Registre aqui' text></Button>
+                        <Button label='Esqueci a senha' text></Button>
+                    </div>
+
+                    <Button label="Sign In" rounded />
                 </div>
             </div>
         </div>
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { LoginPage } from './LoginPage.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
