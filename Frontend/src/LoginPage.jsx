bc1847ade41a639a8e298de51eeac04a027ab8f0
Leonardo Tagliati <leotagliati56@gmail.com>
2025-05-09 13:33:08 -0300

    feat: pagina de login prototipada

--- Frontend/src/LoginPage.jsx
+++ Frontend/src/LoginPage.jsx
@@ -1,18 +1,19 @@
+
 import React from 'react'
 
 export const LoginPage = () => {
     return (
         <div className="container-fluid vh-100">
             <div className="row h-100">
-                <div className="col-md-6 p-0">
+                <div className="bg-primary col p-0">
                     <img
-                        src="https://via.placeholder.com/800x800"
-                        alt="Login Illustration"
+                        src=""
+                        alt="UNDEFINED_Login_Illustration"
                         className="img-fluid h-100 w-100 object-fit-cover"
                     />
                 </div>
 
-                <div className="col-md-6 d-flex flex-column justify-content-center align-items-center bg-light">
+                <div className="col-md-7 d-flex flex-column justify-content-center align-items-center bg-light">
                     <h2 className="mb-4">Bem-vindo de volta</h2>
                     <p>Por favor, entre com suas credenciais para continuar.</p>
                 </div>
