a6377831dbc49253398d43c8661b02e707d621ff
Leonardo Tagliati <leotagliati56@gmail.com>
2025-05-11 20:38:00 -0300

    refactor: melhorando o barramento

--- Backend/barramento-de-eventos/index.js
+++ Backend/barramento-de-eventos/index.js
@@ -3,15 +3,38 @@ const axios = require('axios')
 const app = express()
 app.use(express.json())
 
+const services = {
+    userLoginService: { baseUrl: 'http://localhost', port: 5315 },
+}
+
+const eventRoutes = {
+    UserRegistered: [''],
+    UserLogged: [''],
+
+}
+
 app.post('/event', async (req, res) => {
     const event = req.body
-    try {
-        await axios.post('http://localhost:5050/event', event)
+    const eventType = event.type
+
+    if (eventRoutes[eventType]) {
+        try {
+            const promises = eventRoutes[eventType].map(({ service, path }) => {
+                const { baseUrl, port } = services[service]
+                const url = `${baseUrl}:${port}${path}`
+                return axios.post(url, event)
+            })
+            await Promise.all(promises)
+            console.log(`Evento ${eventType} enviado com sucesso`)
+        } catch (error) {
+            console.log('Erro ao enviar evento para o(s) MS:', err.message)
+        }
     }
-    catch (err) {
-        console.log('Erro ao enviar evento para o ms :', err.message)
+    else {
+        console.log(`Nenhum servico registrado para o evento ${eventType}`)
     }
-    res.end()
+
+    res.status(200).send({ status: 'Evento processado com sucesso' })
 })
 
 const port = 5300
