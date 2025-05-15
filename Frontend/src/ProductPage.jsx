fdf1443c8fc700eac9ea80c7028a3c28523ce71f
Leonardo Tagliati <leotagliati56@gmail.com>
2025-05-15 09:06:28 -0300

    fix: responsividade

--- Frontend/src/ProductPage.jsx
+++ Frontend/src/ProductPage.jsx
@@ -1,30 +1,29 @@
 export function ProductPage({ product }) {
   return (
     <>
-      <div className="container my-4">
-        <div className="d-flex flex-column flex-lg-row border border-2 border-black p-3 gap-4">
+      <div className=" container my-4 ">
+        <div className="d-flex flex-column flex-lg-row border border-2 border-black p-3 gap-3">
           {/* Coluna das imagens */}
-          <div className="col-12 col-md-6 border border-2 border-black p-2 d-flex flex-column">
+          <div className="d-flex border flex-grow-1 border-2 border-black p-2 d-flex flex-column">
             {/* Imagem principal */}
             <div className="d-flex justify-content-center flex-grow-1  mb-3" style={{ minHeight: '300px' }}>
               <img
                 src={product.images[0]}
                 alt="productphoto"
                 className="img-fluid w-100 h-100 border border-2 border-black p-2"
-                style={{ objectFit: 'contain' }}
               />
             </div>
 
             {/* Linha de imagens menores */}
             <div className="border border-2 border-black p-2 overflow-auto">
-              <div className="d-flex flex-row flex-nowrap gap-3">
+              <div className="d-flex  flex-row flex-nowrap gap-3">
                 {Object.entries(product.images).map(([key, url]) => (
                   <div
                     key={key}
                     style={{
                       aspectRatio: '1 / 1',
                       width: '100%',
-                      maxWidth: '120px',
+                      maxWidth: '80px',
                       minWidth: '80px',
                       flex: '0 0 auto',
                     }}
@@ -47,7 +46,7 @@ export function ProductPage({ product }) {
           </div>
 
           {/* Coluna de informações */}
-          <div className="col-12 col-md-6 border border-2 border-black p-3">
+          <div className="border border-2 border-black p-3">
             <h1>{product.name}</h1>
             <p>{product.description}</p>
             <h4 className="text-success">R$ {product.price}</h4>
@@ -71,7 +70,6 @@ export function ProductPage({ product }) {
             <button className="btn btn-primary mt-3">Adicionar ao carrinho</button>
           </div>
         </div>
-
         {/* Avaliações */}
         <div className="mt-4">
           <h4 className="mb-3">Avaliações</h4>
