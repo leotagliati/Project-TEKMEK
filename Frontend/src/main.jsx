cb78cf28f7f6093295044d70f707aa282ba70afe
Leonardo Tagliati <leotagliati56@gmail.com>
2025-05-18 16:54:36 -0300

    chore: mudando para o app.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,28 +7,10 @@ import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import { ProductPage } from './ProductPage.jsx';
 
-// Para aquele que resolveu fazer a sua task, ignora objetos criados nessa classe pq a maioria esta aqui para testar funcionalidade de uma pagina que precisa de referencia para ser criada
-// So ir na root, comentar todos dentro de StrictMode e montar sua task
-const mockProduct = {
-  name: 'Teclado Mecânico RGB',
-  description: 'Teclado mecânico com switches azuis, iluminação RGB e layout ABNT2.',
-  price: 349.90,
-  images: [
-    'url-do-produto-frontal.jpg',
-    'url-do-produto-lateral.jpg',
-    'url-do-produto-traseira.jpg',
-    'url-do-produto-iluminado.jpg'
-  ],
-  misc: {
-    color: ['Carbon-Shell', 'White-Shell'],
-    type: ['fully-assembled', 'barebone'],
-    switches: ['Gateron Jupiter Banana', 'Gateron Jupiter Brown']
-  }
-}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <LoginPage/> */}
-    <ProductPage product={mockProduct} />
+    <App />
   </StrictMode>,
 )
