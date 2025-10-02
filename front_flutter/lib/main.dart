25c7365cea31209b829dc9de8dad5590776aa22c
André Abreu <22.01255-9@maua.br>
2025-10-02 17:21:07 -0300

    chore: estrutura das pastas e google_fonts

--- front_flutter/lib/main.dart
+++ front_flutter/lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:front_flutter/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tekmek',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 255, 255, 255),
-        ),
-      ),
-    );
-  }
+  runApp(const App());
 }
