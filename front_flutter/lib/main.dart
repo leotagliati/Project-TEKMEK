b708e8d2f7e1024d2be1b551ae1f9a1adad7d589
Arthur Lee <138728172+Tutzjwlee@users.noreply.github.com>
2025-11-05 19:33:41 -0300

    feat: added router class and other routes

--- front_flutter/lib/main.dart
+++ front_flutter/lib/main.dart
@@ -4,7 +4,9 @@ import 'package:front_flutter/pages/product/product_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import './utils/router.dart';
+import 'utils/routes_handler.dart';
+import 'package:provider/provider.dart';
+import 'package:front_flutter/utils/auth_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,24 +15,35 @@ void main() async {
   } catch (e) {
     throw Exception('Error loading .env file: $e');
   }
-  runApp(App());
+  final authProvider = AuthProvider();
+  final router = AppRouter.createRouter(authProvider); 
+  runApp(App(authProvider: authProvider, router: router));
 }
 
 class App extends StatelessWidget {
-  App({super.key});
+ 
+  const App({super.key, required this.authProvider, required this.router});
+
+  final AuthProvider authProvider;
+  final GoRouter router;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: "Tekmek",
-      theme: ThemeData(
-        textTheme: GoogleFonts.interTextTheme(),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 255, 255, 255),
+  
+    return ChangeNotifierProvider.value(
+      value: authProvider,
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: "Tekmek",
+        theme: ThemeData(
+          textTheme: GoogleFonts.interTextTheme(),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 255, 255, 255),
+          ),
         ),
+
+        routerConfig: router,
       ),
-      routerConfig: AppRouter.router,
     );
   }
 }
