54f9edcbaec319ed1a50aedd8010eed30d6ddb92
André Abreu <22.01255-9@maua.br>
2025-11-06 00:20:40 -0300

    feat: resultados da pesquisa e redirecionamento

--- front_flutter/lib/common_components/search_component.dart
+++ front_flutter/lib/common_components/search_component.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:front_flutter/api/services.dart';
 import 'package:front_flutter/common_components/product.dart';
+import 'package:front_flutter/common_components/search_product_component.dart';
+import 'package:go_router/go_router.dart';
 
 class SearchComponent extends StatefulWidget {
   const SearchComponent({super.key});
@@ -25,10 +27,46 @@ class _SearchComponentState extends State<SearchComponent> {
       setState(() {
         products = productsList;
       });
-      
     } catch (e) {
       print('Erro ao procurar produtos: $e');
     }
+  }
+
+  // Populando a lista de produtos de maneira fixa
+  // A lista deve ser populada usando um SELECT *
+  // E deve ser repopulada quando clicar no submit utilizando o input como termo de busca
+  @override
+  void initState() {
+    super.initState();
+    products = [];
+    products.add(
+      Product(
+        id: 1,
+        name: 'Keychron K3 Max',
+        price: 499.99,
+        imageUrl: 'https://i.imgur.com/1bBhl4O.png',
+        description:
+            'O K3 Max é um teclado mecânico sem fio discreto com layout de 75%. Ele suporta conexões de 2,4 GHz, Bluetooth e com fio. Com suporte ao QMK/VIA, oferece infinitas possibilidades e maior produtividade no seu trabalho e jogos!',
+        layoutSize: '75%',
+        connectivity: 'Wireless',
+        productType: 'Teclado Mecânico',
+        keycapType: 'Perfil Baixo (Low Profile) ABS',
+      ),
+    );
+    products.add(
+      Product(
+        id: 1,
+        name: 'Keychron K3 Max',
+        price: 499.99,
+        imageUrl: 'https://i.imgur.com/1bBhl4O.png',
+        description:
+            'O K3 Max é um teclado mecânico sem fio discreto com layout de 75%. Ele suporta conexões de 2,4 GHz, Bluetooth e com fio. Com suporte ao QMK/VIA, oferece infinitas possibilidades e maior produtividade no seu trabalho e jogos!',
+        layoutSize: '75%',
+        connectivity: 'Wireless',
+        productType: 'Teclado Mecânico',
+        keycapType: 'Perfil Baixo (Low Profile) ABS',
+      ),
+    );
   }
 
   @override
@@ -36,6 +74,13 @@ class _SearchComponentState extends State<SearchComponent> {
     return SearchAnchor(
       isFullScreen: true,
       searchController: controller,
+      viewLeading: IconButton(
+        onPressed: () {
+          controller.closeView(null);
+        },
+        icon: Icon(Icons.arrow_back),
+        tooltip: 'Voltar',
+      ),
       viewHintText: "Pesquise aqui...",
       viewTrailing: [
         IconButton(
@@ -61,13 +106,13 @@ class _SearchComponentState extends State<SearchComponent> {
         );
       },
       suggestionsBuilder: (BuildContext context, SearchController controller) {
-        return List<ListTile>.generate(5, (int index) {
-          final String item = 'item $index';
+        return List<ListTile>.generate(products.length, (int index) {
           return ListTile(
-            title: Text(item),
+            title: SearchProductComponent(product: products[index]),
             onTap: () {
               setState(() {
-                controller.closeView(item);
+                controller.closeView('');
+                context.go('/product/${products[index].id}');
               });
             },
           );
