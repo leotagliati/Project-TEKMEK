d64512ef1a71974da3aafe5db19c3417f25fd84f
André Abreu <22.01255-9@maua.br>
2025-11-07 01:25:22 -0300

    refactor: refatoração da página do produto

---
+++ front_flutter/lib/pages/product/_compose/product_specs_component.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:front_flutter/models/product.dart';
+
+class ProductSpecsComponent extends StatelessWidget {
+  final Product product;
+
+  const ProductSpecsComponent({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(height: 1, color: Colors.grey[400]),
+        Theme(
+          data: ThemeData().copyWith(dividerColor: Colors.transparent),
+          child: ExpansionTile(
+            title: Text('Descrição'),
+            leading: Icon(Icons.help),
+            iconColor: Color.fromARGB(255, 65, 72, 74),
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 12.0,
+                  right: 12.0,
+                  bottom: 16.0,
+                ),
+                child: Text(product.description),
+              ),
+            ],
+          ),
+        ),
+        Container(height: 1, color: Colors.grey[400]),
+        Theme(
+          data: ThemeData().copyWith(dividerColor: Colors.transparent),
+          child: ExpansionTile(
+            title: Text('Características'),
+            leading: Icon(Icons.brush),
+            iconColor: Color.fromARGB(255, 65, 72, 74),
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 12.0,
+                  right: 12.0,
+                  bottom: 16.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Conectividade: ${product.connectivity} \nLayout: ${product.layoutSize} \nTipo de keycap: ${product.keycapType}',
+                      style: TextStyle(height: 1.5),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
